80b20295425c203eeca0d5e2da26509ce5da1842
Shadow-nCircuit <shadowncircuit@gmail.com>
2025-01-31 11:00:05 -0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,1 @@
-// Function to open the subscribe popup
-window.onload = function() {
-    document.getElementById('subscribe-popup').style.display = 'block';
-};
 
-// Function to close the popup
-function closePopup() {
-    document.getElementById('subscribe-popup').style.display = 'none';
-}
